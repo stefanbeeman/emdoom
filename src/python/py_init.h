8c012910907ec27a647efd1bf4ce5185e6e540f3
Anatole Paine <apaine@quantcast.com>
2014-07-02 01:43:47 -0700

    python_run_file is now a thin wrapper around PyRun_SimpleFile
    This change also fixes a segfault (from a null pointer dereference) when the
    Python file being opened doesn't exist.

--- src/python/py_init.h
+++ src/python/py_init.h
@@ -1,5 +1,5 @@
 void python_start();
 void python_run_string(const char *command);
-void python_run_file(const char *filename);
+void python_run_file(FILE *fp, const char *filename);
 void python_import(const char *name);
 void python_stop();
