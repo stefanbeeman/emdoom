1458528f2ac93277056540180db9c6a38a762183
Anatole Paine <apaine@quantcast.com>
2014-07-13 15:49:21 -0700

    Add skeleton Player class

---
+++ src/python/zdoom/zdplayer.pyx
@@ -0,0 +1,4 @@
+# Placeholder for now
+cdef class Player:
+  def __cinit__(self): pass # Just a wrapper for now
+  def __dealloc__(self): pass # For now, we don't want to screw around with ZDoom's memory...
